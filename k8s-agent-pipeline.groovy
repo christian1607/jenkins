c3fc1419a48c2be67a0d71e9c0b2e4606a74b725
christian altamirano <christian.altamirano.ayala@gmail.com>
2023-01-01 23:30:29 -0500

    containers template for k8s agent

--- k8s-agent-pipeline.groovy
+++ k8s-agent-pipeline.groovy
@@ -1,7 +1,15 @@
-podTemplate {
+podTemplate(containers: [
+    containerTemplate(name: 'maven', image: 'maven:3.8.1-jdk-8', command: 'sleep', args: '99d'),
+    containerTemplate(name: 'golang', image: 'golang:1.16.5', command: 'sleep', args: '99d')
+  ]) {
     node(POD_LABEL) {
-        stage('Run shell') {
-            sh 'echo hello world'
+       stage('Get a Maven project') {
+            git 'https://github.com/jenkinsci/kubernetes-plugin.git'
+            container('maven') {
+                stage('Build a Maven project') {
+                    sh 'mvn -B -ntp clean install'
+                }
+            }
         }
     }
 }
